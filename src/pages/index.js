4c26c4b923edf9678bdc8fcfd5247223cc1eceab
Fabi Lou Sax <contact@fabisax.eu>
2021-06-12 11:52:36 +0200

    remove unused vars

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-import * as style from "./index.module.sass"
-
 const HomePage = () => (
   <Layout>
     <h1>Hello there!</h1>
